a54f690b3c3b3440fae0fb076dac49211606acaf
Hoang Duc Thang <ducthanghoang892004@gmail.com>
2025-08-12 17:37:50 +0700

    45

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,24 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello, World update!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only for user!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only for admin!";
+    }
+
+}
