edb271ec527820b745dac05fde2cfa587841db2c
Hoang Duc Thang <ducthanghoang892004@gmail.com>
2025-08-14 11:03:13 +0700

    first commit_50

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,26 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.hoidanit.laptopshop.service.UserService; // ← Thêm import này
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.getUserList();
+        model.addAttribute("test", test);
+        return "hello";
+    }
+
+}
