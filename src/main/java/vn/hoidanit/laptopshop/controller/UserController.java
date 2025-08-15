f39f6d22ee047ff69397ddd2f8e588895fdd1198
Hoang Duc Thang <ducthanghoang892004@gmail.com>
2025-08-15 17:07:32 +0700

    Send data với HTML from

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,11 +1,14 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.ui.Model;
+import vn.hoidanit.laptopshop.domain.User; // ← THÊM import này
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 import vn.hoidanit.laptopshop.service.UserService; // ← Thêm import này
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class UserController {
@@ -23,9 +26,16 @@ public class UserController {
         return "hello";
     }
 
-    @RequestMapping("/admin/user")
+    @RequestMapping("admin/user")
     public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User()); // Giả sử bạn có một lớp User
         return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
+        System.err.println("run here" + user);
+        return "hello";
     }
 
 }
