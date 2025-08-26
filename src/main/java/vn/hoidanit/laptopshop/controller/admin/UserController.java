814f5368fca08047851d5c5a81d05dafe5369820
Hoang Duc Thang <ducthanghoang892004@gmail.com>
2025-08-26 13:09:12 +0700

    B85 Hoàn thiện Layout Admin

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -1,4 +1,4 @@
-package vn.hoidanit.laptopshop.controller;
+package vn.hoidanit.laptopshop.controller.admin;
 
 import org.springframework.ui.Model;
 import vn.hoidanit.laptopshop.domain.User; // ← THÊM import này
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import vn.hoidanit.laptopshop.repository.UserRepository;
 import java.util.List;
 import vn.hoidanit.laptopshop.service.UserService; // ← Thêm import này
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -38,7 +37,7 @@ public class UserController {
     public String getUserPage(Model model) {
         List<User> users = this.userService.getAllUsers();
         model.addAttribute("users1", users);
-        return "admin/user/table-user";
+        return "admin/user/show";
     }
 
     @RequestMapping("/admin/user/{id}")
@@ -46,7 +45,7 @@ public class UserController {
         User user = this.userService.getUserById(id);
         model.addAttribute("user", user);
         model.addAttribute("id", id);
-        return "admin/user/show";
+        return "admin/user/detail";
     }
 
     @RequestMapping("/admin/user/create")
